7b4795462ddeb2fbe2f98ed458bcd7971d0379cc
Neon-Leon1927 <kathryella@gmail.com>
2026-01-21 10:57:31 +0300

    Добавлен класс DatabaseHelper

---
+++ AppGlobus/DatabaseHelper.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+
+namespace AppGlobus
+{
+    public static class DatabaseHelper
+    {
+        // Исправляем: одна простая строка подключения
+        public static string ConnectionString =>
+            @"Server=(localdb)\MSSQLLocalDB;Database=Globusbd;Integrated Security=True;";
+
+        public static bool TestConnection()
+        {
+            try
+            {
+                using (var connection = new SqlConnection(ConnectionString))
+                {
+                    connection.Open();
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка подключения: {ex.Message}", "Ошибка БД");
+                return false;
+            }
+        }
+
+
+    }
+}
